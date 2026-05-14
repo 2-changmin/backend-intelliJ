3f633e6a7ed8a491e5e498fedf05462e5c7d6a96
2-changmin <vksek1021@naver.com>
2026-05-14 15:50:09 +0900

    RequestParam 이용

--- src/main/java/org/example/memo/controller/MemoController.java
+++ src/main/java/org/example/memo/controller/MemoController.java
@@ -2,6 +2,7 @@ package org.example.memo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +12,11 @@ public class MemoController {
     @GetMapping
     public String test(){
         return "hello";
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam String name){
+        return "안녕하세요 " + name + "님";
     }
 
 
